d24982b930bd6b0b20f768db2c6efdad45757021
Daniel <marmandu@gmail.com>
2023-11-24 16:48:29 +0000

    Create new dropdown menu to select a plot in a given selected study. make the fetched study data available for futute operations over it

--- lib/grassroots_studies.dart
+++ lib/grassroots_studies.dart
@@ -17,12 +17,16 @@ class _GrassrootsPageState extends State<GrassrootsStudies> {
   String? address;
   String? FTrial;
   int numberOfPlots = 0;
-  List<String> plotIDList = [];
+  String? selectedPlotId;
+  List<String> plotIDs = [];
+  List<String> plotDisplayValues = [];
+  String? selectedPlotDisplayValue;
+  Map<String, dynamic>? fetchedStudyDetails;
 
   @override
   void initState() {
     super.initState();
-    fetchStudies(); // Updated to call the new method
+    fetchStudies(); // Updated to call the new method to fetch all studies
   }
 
   void fetchStudies() async {
@@ -67,37 +71,54 @@ class _GrassrootsPageState extends State<GrassrootsStudies> {
                             setState(() {
                               selectedStudy = newValue;
                               isSingleStudyLoading = true; // Start loading
+                              // Reset plot lists
+                              plotIDs.clear();
+                              plotDisplayValues.clear();
+                              selectedPlotId = null; // Also reset the selected plot ID
+                              selectedPlotDisplayValue = null;
                             });
 
                             try {
                               var studyDetails = await QRCodeService.fetchSingleStudy(newValue!);
 
+                              //setState(() {
+                              //  fetchedStudyDetails = studyDetails; // Store the fetched details
+                              //});
+
                               // Check if 'plots' exists and is not null
                               if (studyDetails['results'][0]['results'][0]['data'].containsKey('plots') &&
                                   studyDetails['results'][0]['results'][0]['data']['plots'] != null) {
                                 var plots = studyDetails['results'][0]['results'][0]['data']['plots'] as List<dynamic>;
-                                List<String> plotIDs = [];
+                                //List<String> tempPlotIDs = [];
+                                //List<String> tempPlotDisplayValues = []; // Will hold plot indices for display
                                 int nPlots = 0;
 
                                 for (var plot in plots) {
-                                  if (plot['rows'] != null && plot['rows'].isNotEmpty) {
+                                  if (plot.containsKey('rows') && plot['rows'] is List && plot['rows'].isNotEmpty) {
                                     var row = plot['rows'][0];
                                     if (!(row.containsKey('discard') || row.containsKey('blank'))) {
                                       String plotID = row['_id']['\$oid'];
+                                      String plotIndex = row['study_index']
+                                          .toString(); // Assuming study_index is the value you want to display
+                                      //tempPlotIDs.add(plotID);
+                                      //tempPlotDisplayValues.add(plotIndex);
                                       plotIDs.add(plotID);
+                                      plotDisplayValues.add(plotIndex);
                                       nPlots++;
                                     }
                                   }
                                 }
-
                                 // Update the state with the number of plots and their IDs
                                 setState(() {
                                   numberOfPlots = nPlots;
-                                  plotIDList = plotIDs;
+                                  fetchedStudyDetails = studyDetails; // Store the fetched details
+                                  //plotIDs = tempPlotIDs; // Update class-level variable
+                                  //plotDisplayValues = tempPlotDisplayValues; // Update class-level variable
                                 });
 
                                 print('Number of Plots: $nPlots');
                                 print('Plot IDs: $plotIDs');
+                                print('Plot Display Values: $plotDisplayValues');
                               }
 
                               // Updating the state with other study details
@@ -176,6 +197,60 @@ class _GrassrootsPageState extends State<GrassrootsStudies> {
                               'Number of Plots: $numberOfPlots',
                               style: TextStyle(fontSize: 16),
                             ),
+                          ],
+                          SizedBox(height: 20),
+                          if (plotDisplayValues.isNotEmpty) ...[
+                            DropdownButtonFormField<String>(
+                              isExpanded: true,
+                              value: selectedPlotId,
+                              hint: Text("Select a plot"),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedPlotId = newValue;
+                                  int index = plotIDs.indexOf(newValue!);
+                                  if (index != -1) {
+                                    selectedPlotDisplayValue = plotDisplayValues[index];
+                                    ///////////// Additional logic when a plot is selected /////
+                                    /// Example: Count the number of observations in the selected plot
+                                    var plots = fetchedStudyDetails!['results'][0]['results'][0]['data']['plots']
+                                        as List<dynamic>;
+
+                                    // Find the plot that matches the selectedPlotId
+                                    var selectedPlot = plots.firstWhere(
+                                      (plot) =>
+                                          plot['rows'] != null && plot['rows'][0]['_id']['\$oid'] == selectedPlotId,
+                                      orElse: () => null,
+                                    );
+
+                                    if (selectedPlot != null) {
+                                      // Count the number of observations
+                                      var observationCount = selectedPlot['rows'][0]['observations'] != null
+                                          ? selectedPlot['rows'][0]['observations'].length
+                                          : 0;
+
+                                      print('Number of Observations in the selected plot: $observationCount');
+                                    }
+                                  }
+                                });
+                                // Additional logic when a plot is selected, if needed
+                                print('Selected Plot ID: $newValue'); // Print the actual plot ID to console
+                              },
+                              items: List<DropdownMenuItem<String>>.generate(
+                                plotDisplayValues.length,
+                                (index) => DropdownMenuItem<String>(
+                                  value: plotIDs[index],
+                                  child: Text(plotDisplayValues[index]),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20), // Optional spacing
+                            if (selectedPlotDisplayValue != null) ...[
+                              SizedBox(height: 10),
+                              Text(
+                                'Selected plot: $selectedPlotDisplayValue',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
                           ],
                         ],
                       ],
